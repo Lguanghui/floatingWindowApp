db14c12c35c7aff9b9fca5914591adbd4619c620
liangguanghui <liangguanghui@keep.com>
2021-09-01 22:05:32 +0800

    工程配置

---
+++ Podfile
@@ -0,0 +1,13 @@
+source 'https://github.com/CocoaPods/Specs.git'
+use_frameworks!
+
+def import_pods
+    pod 'SwiftyJSON', '~> 4.0'
+    pod 'SnapKit', '~> 5.0.0'
+    pod 'Masonry', '~> 1.1.0'
+end
+
+target 'myApp1' do
+    platform :ios, '14.0'
+    import_pods
+end
--- myApp1/ViewController.swift
+++ myApp1/ViewController.swift
@@ -6,14 +6,33 @@
 //
 
 import UIKit
+import SnapKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupViews()
     }
 
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+    }
+    
+    func setupViews() {
+        let mainView = UIView()
+        
+        self.view.addSubview(mainView)
+        
+        mainView.snp.makeConstraints { view in
+            view 
+        }
+        
+        mainView.snp_makeConstraints { view in
+            view.bottom.top.left.right.equalToSuperview()
+        }
+        mainView.backgroundColor = .orange
+    }
 }
 
